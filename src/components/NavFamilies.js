9fe0f410dd55ef40b203bc51d69f2290491b8e38
Carlito <rody.castillo.tacuri@gmail.com>
2022-02-14 16:19:37 -0500

    [feat]: add slider in families navbar

--- src/components/NavFamilies.js
+++ src/components/NavFamilies.js
@@ -1,212 +1,135 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Navbar from "./Navbar";
+import { SliderNavbar } from "./SliderNavbar";
 import "./styles/style.css";
 
 export const NavFamilies = () => {
   const width = window.innerWidth;
   return (
     <>
-    <section>
-    {/*  */}
-    {
-      width <= 992 ? (
-        <section className="position-fixed w-100 top-0" style={{zIndex: "1000"}}>
-        <Navbar/>
-        <nav className="navbar navbar-expand-lg navbar-dark navbar-brands navbar-brands-lg" style={{marginTop: "120px"}}>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navfamilies"
-          aria-controls="navfamilies"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse  nav-families"
-          id="navfamilies"
-        >
-          <ul className="navbar-nav">
-            <li className="nav-item-fam">
-              <Link className="brands-link" to="/familia/agenciadolphintravel">
-                <p className="text-center">
-                  AGENCIA <br />
-                  DOLPHIN TRAVEL
-                </p>
-              </Link>
-            </li>
-            <li className="nav-item-fam">
-              <Link className="brands-link" to="/familia/finca314">
-                <p className="text-center">
-                  Finca <br />
-                  314
-                </p>
-              </Link>
-            </li>
-            <li className="nav-item-fam">
-              <Link className="brands-link" to="/familia/bodegasantalucia">
-                <p className="text-center text-uppercase">
-                  BODEGA <br />
-                  SANTA LUCíA
-                </p>
-              </Link>
-            </li>
-            <li className="nav-item-fam">
-              <Link className="brands-link" to="/familia/bodegasannicolas">
-                <p className="text-center text-uppercase">
-                  Bodega <br />
-                  SAN Nicolás
-                </p>
-              </Link>
-            </li>
-            <li className="nav-item-fam">
-              <Link className="brands-link" to="/familia/bodegaelcatador">
-                <p className="text-center">
-                  Bodega <br />
-                  el catador
-                </p>
-              </Link>
-            </li>
-            <li className="nav-item-fam">
-              <Link className="brands-link" to="/familia/bodegapampasdeica">
-                <p className="text-center">
-                  Bodega <br />
-                  pampas de ica
-                </p>
-              </Link>
-            </li>
-            <li className="nav-item-fam">
-              <Link className="brands-link" to="/familia/bodegatresgeneraciones">
-                <p className="text-center">
-                  Bodega <br />
-                  tres Generaciones
-                </p>
-              </Link>
-            </li>
-            <li className="nav-item-fam">
-              <Link className="brands-link" to="/familia/bodegavillacuri">
-                <p className="text-center">
-                  Bodega <br />
-                  Villacuri
-                </p>
-              </Link>
-            </li>
-            <li className="nav-item-fam">
-              <Link className="brands-link" to="/familia/haciendagranpaso">
-                <p className="text-center">
-                  Hacienda <br />
-                  gran paso
-                </p>
-              </Link>
-            </li>
-            <li className="nav-item-fam">
-              <Link className="brands-link" to="/familia/restaurantelaolladejuanita">
-                <p className="text-center">
-                  Restaurante <br />
-                  La olla de juanita
-                </p>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <section>
+        {/*  */}
+        {width <= 992 ? (
+          <section
+            className="position-fixed w-100 top-0"
+            style={{ zIndex: "1000" }}
+          >
+            <Navbar />
+            <SliderNavbar />
+          </section>
+        ) : (
+          <section
+            className="position-fixed w-100 top-0"
+            style={{ zIndex: "1000" }}
+          >
+            <Navbar />
+            <nav className="navbar-brands" style={{ marginTop: "120px" }}>
+              <ul
+                className="d-flex justify-content-around  align-items-center"
+                style={{ marginBottom: "0" }}
+              >
+                <li className="nav-item-fam">
+                  <NavLink
+                    className="brands-link"
+                    to="/familia/agenciadolphintravel"
+                  >
+                    <p className="text-center">
+                      AGENCIA <br />
+                      DOLPHIN TRAVEL
+                    </p>
+                  </NavLink>
+                </li>
+                <li className="nav-item-fam">
+                  <NavLink className="brands-link" to="/familia/finca314">
+                    <p className="text-center">
+                      Finca <br />
+                      314
+                    </p>
+                  </NavLink>
+                </li>
+                <li className="nav-item-fam">
+                  <NavLink
+                    className="brands-link"
+                    to="/familia/bodegasantalucia"
+                  >
+                    <p className="text-center">
+                      BODEGA <br />
+                      SANTA LUCíA
+                    </p>
+                  </NavLink>
+                </li>
+                <li className="nav-item-fam">
+                  <NavLink
+                    className="brands-link"
+                    to="/familia/bodegasannicolas"
+                  >
+                    <p className="text-center">
+                      Bodega <br />
+                      San Nicolás
+                    </p>
+                  </NavLink>
+                </li>
+                <li className="nav-item-fam">
+                  <NavLink
+                    className="brands-link"
+                    to="/familia/bodegaelcatador"
+                  >
+                    <p className="text-center">
+                      Bodega <br />
+                      el catador
+                    </p>
+                  </NavLink>
+                </li>
+                <li className="nav-item-fam">
+                  <NavLink
+                    className="brands-link"
+                    to="/familia/bodegapampasdeica"
+                  >
+                    <p className="text-center">
+                      Bodega <br />
+                      pampas de ica
+                    </p>
+                  </NavLink>
+                </li>
+                <li className="nav-item-fam">
+                  <NavLink
+                    className="brands-link"
+                    to="/familia/bodegatresgeneraciones"
+                  >
+                    <p className="text-center">
+                      Bodega <br />
+                      tres Generaciones
+                    </p>
+                  </NavLink>
+                </li>
+                <li className="nav-item-fam">
+                  <NavLink
+                    className="brands-link"
+                    to="/familia/bodegavillacuri"
+                  >
+                    <p className="text-center">
+                      Bodega <br />
+                      Villacuri
+                    </p>
+                  </NavLink>
+                </li>
+                <li className="nav-item-fam">
+                  <NavLink
+                    className="brands-link"
+                    to="/familia/restaurantelaolladejuanita"
+                  >
+                    <p className="text-center">
+                      Restaurante <br />
+                      La olla de juanita
+                    </p>
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </section>
+        )}
       </section>
-      ) : (
-        <section className="position-fixed w-100 top-0" style={{zIndex: "1000"}}>
-          <Navbar/>
-          <nav className="navbar-brands" style={{marginTop: "120px"}}>
-        <ul
-          className="d-flex justify-content-around  align-items-center"
-          style={{ marginBottom: "0" }}
-        >
-          <li className="nav-item-fam">
-            <NavLink className="brands-link" to="/familia/agenciadolphintravel">
-              <p className="text-center">
-                AGENCIA <br />
-                DOLPHIN TRAVEL
-              </p>
-            </NavLink>
-          </li>
-          <li className="nav-item-fam">
-            <NavLink className="brands-link" to="/familia/finca314">
-              <p className="text-center">
-                Finca <br />
-                314
-              </p>
-            </NavLink>
-          </li>
-          <li className="nav-item-fam">
-            <NavLink className="brands-link" to="/familia/bodegasantalucia">
-              <p className="text-center">
-                BODEGA <br />
-                SANTA LUCíA
-              </p>
-            </NavLink>
-          </li>
-          <li className="nav-item-fam">
-            <NavLink className="brands-link" to="/familia/bodegasannicolas">
-              <p className="text-center">
-                Bodega <br />
-                San Nicolás
-              </p>
-            </NavLink>
-          </li>
-          <li className="nav-item-fam">
-            <NavLink className="brands-link" to="/familia/bodegaelcatador">
-              <p className="text-center">
-                Bodega <br />
-                el catador
-              </p>
-            </NavLink>
-          </li>
-          <li className="nav-item-fam">
-            <NavLink className="brands-link" to="/familia/bodegapampasdeica">
-              <p className="text-center">
-                Bodega <br />
-                pampas de ica
-              </p>
-            </NavLink>
-          </li>
-          <li className="nav-item-fam">
-            <NavLink className="brands-link" to="/familia/bodegatresgeneraciones">
-              <p className="text-center">
-                Bodega <br />
-                tres Generaciones
-              </p>
-            </NavLink>
-          </li>
-          <li className="nav-item-fam">
-            <NavLink className="brands-link" to="/familia/bodegavillacuri">
-              <p className="text-center">
-                Bodega <br />
-                Villacuri
-              </p>
-            </NavLink>
-          </li>
-          <li className="nav-item-fam">
-            <NavLink
-              className="brands-link"
-              to="/familia/restaurantelaolladejuanita"
-            >
-              <p className="text-center">
-                Restaurante <br />
-                 La olla de juanita
-              </p>
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-   
-        </section>
- 
-      )
-    }
-      
-      
-    </section>
     </>
   );
 };
---
+++ src/components/SliderNavbar.js
@@ -0,0 +1,185 @@
+import React, { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from "swiper";
+import "./styles/style.css";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "swiper/swiper-bundle.css";
+import { NavLink } from "react-router-dom";
+
+// imgs
+import ArrowRight from "../assets/img/home/arrow-right.png";
+import ArrowLeft from "../assets/img/home/arrow-left.png";
+
+SwiperCore.use([Navigation]);
+
+export const SliderNavbar = () => {
+  const swiperRef = useRef(null);
+  //   const routes = [
+  //     { id: 1, data: "dolphin1" },
+  //     { id: 2, data: "dolphin2" },
+  //     { id: 3, data: "dolphin3" },
+  //     { id: 4, data: "dolphin4" },
+  //     { id: 5, data: "dolphin5" },
+  //     { id: 6, data: "dolphin6" },
+  //     { id: 7, data: "dolphin7" },
+  //   ];
+  return (
+    <div style={{ marginTop: "120px" }}>
+      <Swiper
+        ref={swiperRef}
+        spaceBetween={0}
+        slidesPerView={2}
+        // breakpoints={{
+        //   540: {
+        //     slidesPerView: 2,
+        //   },
+        //   992: {
+        //     slidesPerView: 4,
+        //   },
+        // }}
+      >
+        <SwiperSlide>
+          <div>
+            <NavLink
+              className="nav-link brands-link text-decoration-none"
+              to="/familia/agenciadolphintravel"
+            >
+              <p className="text-center">
+                AGENCIA <br />
+                DOLPHIN TRAVEL
+              </p>
+            </NavLink>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <NavLink className="nav-link brands-link" to="/familia/finca314">
+              <p className="text-center">
+                Finca <br />
+                314
+              </p>
+            </NavLink>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <NavLink
+              className="nav-link brands-link"
+              to="/familia/bodegasantalucia"
+            >
+              <p className="text-center text-uppercase">
+                BODEGA <br />
+                SANTA LUCíA
+              </p>
+            </NavLink>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <NavLink
+              className="nav-link brands-link"
+              to="/familia/bodegasannicolas"
+            >
+              <p className="text-center text-uppercase">
+                Bodega <br />
+                SAN Nicolás
+              </p>
+            </NavLink>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <NavLink
+              className="nav-link brands-link"
+              to="/familia/bodegaelcatador"
+            >
+              <p className="text-center">
+                Bodega <br />
+                el catador
+              </p>
+            </NavLink>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <NavLink
+              className="nav-link brands-link"
+              to="/familia/bodegapampasdeica"
+            >
+              <p className="text-center">
+                Bodega <br />
+                pampas de ica
+              </p>
+            </NavLink>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <NavLink
+              className="nav-link brands-link"
+              to="/familia/bodegatresgeneraciones"
+            >
+              <p className="text-center">
+                Bodega <br />
+                tres Generaciones
+              </p>
+            </NavLink>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <NavLink
+              className="nav-link brands-link"
+              to="/familia/bodegavillacuri"
+            >
+              <p className="text-center">
+                Bodega <br />
+                Villacuri
+              </p>
+            </NavLink>
+          </div>
+        </SwiperSlide>
+        {/* <SwiperSlide>
+          <div>
+            <NavLink className="brands-link" to="/familia/haciendagranpaso">
+              <p className="text-center">
+                Hacienda <br />
+                gran paso
+              </p>
+            </NavLink>
+          </div>
+        </SwiperSlide> */}
+        <SwiperSlide>
+          <div>
+            <NavLink
+              className="nav-link brands-link"
+              to="/familia/restaurantelaolladejuanita"
+            >
+              <p className="text-center">
+                Restaurante <br />
+                La olla de juanita
+              </p>
+            </NavLink>
+          </div>
+        </SwiperSlide>
+        <div
+          id="previousButton"
+          className="button-prev"
+          style={{ color: "#000", left: 0 }}
+          onClick={() => swiperRef.current.swiper.slidePrev()}
+        >
+          <img src={ArrowLeft} alt="arrow-left" style={{ maxHeight: 48 }} />
+        </div>
+        <div
+          id="nextButton"
+          className=" button-next"
+          style={{ color: "#000", right: 0 }}
+          onClick={() => swiperRef.current.swiper.slideNext()}
+        >
+          <img src={ArrowRight} alt="arrow-right" style={{ maxHeight: 48 }} />
+        </div>
+      </Swiper>
+    </div>
+  );
+};
